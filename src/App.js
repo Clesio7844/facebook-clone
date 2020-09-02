ae6181c60b3757c3bcbeb8bda2d1264efe29c18b
david <daviddecastro42@live.co.uk>
2020-09-02 01:38:02 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    // BEM naming conversation
+    <div className='app'>
+      <h1>Lets build the facebook clone!</h1>
+
+      <Header />
+
+      {/* App body */}
+      {/* Sidebar */}
+      {/* Feed */}
+      {/* Widgets */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyA_1xJdvUaeS7cBP3LJlcn3oCk3PDtUtwo',
+  authDomain: 'facebook-clone-ec109.firebaseapp.com',
+  databaseURL: 'https://facebook-clone-ec109.firebaseio.com',
+  projectId: 'facebook-clone-ec109',
+  storageBucket: 'facebook-clone-ec109.appspot.com',
+  messagingSenderId: '234070912872',
+  appId: '1:234070912872:web:e741706be6cd153434b0dd',
+  measurementId: 'G-7K23361WJ8'
+};
