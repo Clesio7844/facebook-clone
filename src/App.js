f79f03282efd93a721a78c6e3290b115a7c1d1b5
david <daviddecastro42@live.co.uk>
2020-09-03 03:09:33 +0100

    finished component feed page and css page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import './App.css';
 import Header from './Header';
 import Sidebar from './Sidebar';
+import Feed from './Feed';
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
       <Header />
       <div className='app__body'>
         <Sidebar />
-        {/* Sidebar */}
+        <Feed />
         {/* Feed */}
         {/* Widgets */}
       </div>
